3f35639fdb5e870a28ed720eb407210881caa737
SrTesch <pedrotesch.pt@gmail.com>
2024-08-14 22:37:48 -0300

    just starting

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Footer from "./components/footer"
+function App() {
+
+  return (
+    <div>
+    <h1>Wb Sat</h1>
+    <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./footer.module.css"
+
+export default function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <h1>teste</h1>
+        </footer>
+    )
+}
