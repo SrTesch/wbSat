d974f803ed0fae5eb32bbe719d0bd5a539d239fd
SrTesch <pedrotesch.pt@gmail.com>
2024-09-17 17:09:16 -0300

    adding some high quality logos on products and updating the temporary photo

---
+++ src/components/products/productsContent.ts
@@ -0,0 +1,35 @@
+
+import OntrackingImage from "../../assets/ontracking.png"
+import apptrackerImage from "../../assets/appTracker.png"
+import onAlertImage from "../../assets/onAlert.png"
+import onPatrolImage from "../../assets/onPatrol.png"
+import onWallImage from "../../assets/onWall.png"
+let cardsContent = [
+    {
+        titulo:"OnTracking",
+        img: OntrackingImage,
+        texto: "Plataforma de rastreamento por GPS"
+    },
+    {
+        titulo:"App Tracker",
+        img: apptrackerImage,
+        texto: "Digitalização de tarefas e serviços de campo"   
+    },
+    {
+        titulo:"OnAlert",
+        img: onAlertImage,
+        texto: "Centro de Monitoramento"
+    },
+    {
+        titulo:"OnPatrol",
+        img: onPatrolImage,
+        texto: "Gestão e controle de Rondas de Segurança"
+    },
+    {
+        titulo:"OnWall",
+        img: onWallImage,
+        texto: "Monitoramento de múltiplos ativos"
+    }
+]
+
+export default cardsContent;
