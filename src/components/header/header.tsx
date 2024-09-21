e5131fa52e02effbfbae8bca25c747d0b1e6a235
SrTesch <pedrotesch.pt@gmail.com>
2024-09-21 01:42:10 -0300

    fixing header bug and improve more responsive details for apptracker component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,24 +1,30 @@
 import { useState } from "react";
-import "./header.css"
-import Logo from '../../assets/logo.png'
-export default function Header(){
-    const [ativar, setAtivar] = useState("nav-list");
-        const navList = () => {
-            ativar === 'nav-list' ? setAtivar('nav-list nav-active') : setAtivar("nav-list");
-        }
+import "./header.css";
+import Logo from '../../assets/logo.png';
+
+export default function Header() {
+    const [menuAtivo, setMenuAtivo] = useState(false); // Usar um booleano para simplificar
+
+    const navList = () => {
+        setMenuAtivo(!menuAtivo); // Alterna entre true e false
+        console.log("Menu ativo: ", menuAtivo); // Verifique o valor no console
+    };
+
     return (
         <header className='header'>
             <div className='limitar-secao navbar'>
                 <div className='nome-topo'>
-                    <a className='link-nome' href='#home'><img src={Logo} className="eusoulindo"alt="" /></a>
+                    <a className='link-nome' href='#home'><img src={Logo} className="eusoulindo" alt="Logo" /></a>
                 </div>
                 <nav>
+                    {/* Botão hamburger */}
                     <div className="mobile-menu" onClick={navList}>
                         <div className='line1'></div>
-                        <div className='line2'></div>
-                        <div className='line2'></div>
+                        <div className='line1'></div>
+                        <div className='line1'></div>
                     </div>
-                    <div className={ativar}>
+                    {/* Classe alterna com base no estado */}
+                    <div className={menuAtivo ? 'nav-list nav-active' : 'nav-list'}>
                         <a href='#home' className='link-topo link-home'>Home</a>
                         <a href='#products' className='link-topo'>Serviços</a>
                         <a href='#footer' className='link-topo'>Contato</a>
@@ -26,5 +32,5 @@ export default function Header(){
                 </nav>
             </div>
         </header>
-    )
+    );
 }
